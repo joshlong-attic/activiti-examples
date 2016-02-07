c7580fa934c2046d06bd126c411dec44244311e5
Josh Long <josh@joshlong.com>
2016-02-07 08:15:18 +0100

    slight polish

--- async-activiti-integration/src/main/java/com/example/DemoApplication.java
+++ async-activiti-integration/src/main/java/com/example/DemoApplication.java
@@ -2,116 +2,114 @@ package com.example;
 
 import org.activiti.engine.ProcessEngine;
 import org.activiti.engine.impl.bpmn.behavior.ReceiveTaskActivityBehavior;
+import org.activiti.engine.impl.pvm.delegate.ActivityBehavior;
 import org.activiti.engine.impl.pvm.delegate.ActivityExecution;
+import org.activiti.engine.runtime.ProcessInstance;
 import org.activiti.spring.integration.Activiti;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.integration.channel.DirectChannel;
 import org.springframework.integration.dsl.IntegrationFlow;
 import org.springframework.integration.dsl.IntegrationFlows;
 import org.springframework.integration.support.MessageBuilder;
 import org.springframework.messaging.Message;
-import org.springframework.messaging.MessageChannel;
-import org.springframework.messaging.MessageHeaders;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-/**
- * This example demonstrates how to launch an Activit workflow process that then kicks of a Spring Integration flow that
- * returns in an out-of-band request (potentially minutes, hours, or years later). This doesn't block: messages
- * are forwarded to Spring Integration where they may do anything they want. Only a subsequent message
- * containing the {@code executionId} header signal's the execution of the process.
- *
- * @author Josh Long
- */
+import java.util.Collections;
+import java.util.Map;
+
 @SpringBootApplication
 public class DemoApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
 
-    @RestController
-    public static class ProcessRestController {
+	private Log log = LogFactory.getLog(getClass());
 
-        @Autowired
-        private ProcessEngine processEngine;
+	@Bean
+	ActivityBehavior gateway(MessageChannels channels) {
+		return new ReceiveTaskActivityBehavior() {
 
-        @Autowired
-        @Qualifier("resume")
-        private MessageChannel resume;
+			@Override
+			public void execute(ActivityExecution execution) throws Exception {
 
-        @RequestMapping(method = RequestMethod.GET, value = "/resume/{executionId}")
-        public void resume(@PathVariable String executionId) {
-            Message<String> build = MessageBuilder.withPayload(executionId)
-                    .setHeader("executionId", executionId).build();
-            this.resume.send(build);
-        }
+				Message<?> executionMessage = MessageBuilder
+						.withPayload(execution)
+						.setHeader("executionId", execution.getId())
+						.build();
 
-        @RequestMapping(method = RequestMethod.GET, value = "/start")
-        public void launch() {
-            this.processEngine.getRuntimeService()
-                    .startProcessInstanceByKey("asyncProcess");
-        }
-    }
+				channels.requests().send(executionMessage);
+			}
+		};
+	}
 
-    // requests on this channel are _FROM_ Activiti process ActivityBehavior _TO_ Spring Integration handler
-    @Bean
-    DirectChannel delegate() {
-        return new DirectChannel();
-    }
+	@Bean
+	IntegrationFlow requestsFlow(MessageChannels channels) {
+		return IntegrationFlows.from(channels.requests())
+				.handle(msg -> msg.getHeaders().entrySet()
+						.forEach(e -> log.info(e.getKey() + '=' + e.getValue())))
+				.get();
+	}
 
-    // requests on this channel are _FROM_ Spring MVC REST controller _TO_ Activiti process
-    @Bean
-    DirectChannel resume() {
-        return new DirectChannel();
-    }
+	@Bean
+	IntegrationFlow repliesFlow(MessageChannels channels,
+								ProcessEngine engine) {
+		return IntegrationFlows.from(channels.replies())
+				.handle(msg -> engine.getRuntimeService().signal(
+						String.class.cast(msg.getHeaders().get("executionId"))))
+				.get();
+	}
+}
 
-    @Bean
-    ReceiveTaskActivityBehavior gateway() {
 
-        MessageChannel channel = this.delegate();
+@Configuration
+class MessageChannels {
 
-        return new ReceiveTaskActivityBehavior() {
-            @Override
-            public void execute(ActivityExecution execution) throws Exception {
+	@Bean
+	DirectChannel requests() {
+		return new DirectChannel();
+	}
 
-                Message<?> executionMessage = MessageBuilder
-                        .withPayload(execution)
-                        .setHeader("executionId", execution.getId())
-                        .build();
+	@Bean
+	DirectChannel replies() {
+		return new DirectChannel();
+	}
+}
 
-                channel.send(executionMessage);
-            }
-        };
-    }
+@RestController
+class ProcessStartingRestController {
 
-    // from Activiti process to Spring Integration
-    @Bean
-    IntegrationFlow from() {
-        return IntegrationFlows.from(this.delegate())
-                .handle(msg -> {
-                    msg.getHeaders()
-                            .keySet()
-                            .forEach(k -> {
-                                MessageHeaders headers = msg.getHeaders();
-                                System.out.println(String.format("%s = %s", k, headers.get(k)));
-                            });
-                    System.out.println("executionId = " + msg.getPayload());
-                })
-                .get();
-    }
+	@Autowired
+	private ProcessEngine processEngine;
 
-    // from Spring Integration to Activiti
-    @Bean
-    IntegrationFlow to(ProcessEngine engine) {
-        return IntegrationFlows.from(resume())
-                .handle(Activiti.signallingMessageHandler(engine))
-                .get();
-    }
+	@RequestMapping(method = RequestMethod.GET, value = "/start")
+	Map<String, String> launch() {
+		ProcessInstance asyncProcess = this.processEngine.getRuntimeService()
+				.startProcessInstanceByKey("asyncProcess");
+		return Collections.singletonMap("executionId", asyncProcess.getId());
+	}
+}
+
+@RestController
+class ProcessResumingRestController {
+
+	@Autowired
+	private MessageChannels messageChannels;
+
+	@RequestMapping(method = RequestMethod.GET, value = "/resume/{executionId}")
+	void resume(@PathVariable String executionId) {
+		Message<String> build = MessageBuilder.withPayload(executionId)
+				.setHeader("executionId", executionId)
+				.build();
+		this.messageChannels.replies().send(build);
+	}
 }
